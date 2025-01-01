3438ab018b370434b7210b2f6b78eba41f738852
kimo <kimografico@gmail.com>
2025-01-01 19:08:37 +0100

    Practica 2 segunda parte

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -8,6 +8,8 @@ window.addEventListener("load", (ev) => {
   let user = document.getElementById("user");
   let boton = document.querySelector("button");
 
+  let userData;
+
   boton.addEventListener("click", (ev) => {
     ev.preventDefault();
     clearFields();
@@ -24,13 +26,23 @@ function clearFields() {
 
 async function procesarFetch(numsecs, user) {
   try {
+    if (isNaN(numsecs) || numsecs <= 0) {
+      document.getElementById("status").innerText =
+        "⚠️ El número de segundos debe ser un valor positivo";
+      throw new Error("⚠️ El número de segundos debe ser un valor positivo");
+    }
+
     const response = await fetch(BASE_URL + user);
+    statusCode = response.status;
     if (!response.ok) {
+      document.getElementById("status").innerText = "⚠️ " + statusCode;
       throw new Error(response.status);
     }
 
     let datos = await response.json();
     datos = datos.data;
+
+    userData = datos;
 
     if (datos) {
       setTimeout(() => {
@@ -38,11 +50,34 @@ async function procesarFetch(numsecs, user) {
         document.getElementById("email").innerText = datos.email;
         document.getElementById("name").innerText = datos.first_name;
         document.getElementById("status").innerText = response.status;
+        postData();
       }, numsecs * 1000);
     } else {
+      document.getElementById("status").innerText = "⚠️ " + statusCode;
       throw new Error("⚠️ Datos no encontrados");
     }
   } catch (error) {
     console.error(error);
+    if (isNaN(user) || user > 12 || user <= 0) {
+      document.getElementById("status").innerText = "⚠️ " + statusCode;
+      throw new Error("ERROR ⚠️ El usuario debe ser un número entre 1 y 12");
+    }
+  }
+}
+
+async function postData() {
+  try {
+    const response = await fetch(POSTMAN_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    const data = await response.json();
+  } catch (error) {
+    document.getElementById("status").innerText = "⚠️ " + error;
+    console.error("Error:", error);
   }
 }
